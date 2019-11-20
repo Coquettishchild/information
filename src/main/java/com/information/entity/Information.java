5d2d3facad0253165731e848e89d43d7cf8caed4
Archiver <302866617@qq.com>
2019-11-20 11:09:56 +0800

    thymeleaf

--- src/main/java/com/information/entity/Information.java
+++ src/main/java/com/information/entity/Information.java
@@ -1,5 +1,7 @@
 package com.information.entity;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
@@ -16,6 +18,7 @@ import java.util.Date;
 @TableName("information")
 public class Information {
     //id
+    @TableId(value = "id",type = IdType.AUTO)
     private String id;
     //姓名
     private String name;
