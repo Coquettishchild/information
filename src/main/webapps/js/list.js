62ad0525ff10e8a8d9da46006f5edad7f60044b6
Archiver <302866617@qq.com>
2019-11-21 19:49:02 +0800

    提交

--- src/main/webapps/js/list.js
+++ src/main/webapps/js/list.js
@@ -1,3 +1,18 @@
+window.onload=function(){
+    list();
+}
+
+var list = function(pageNo){
+
+    $.ajax({
+        url: "/info/page/"+pageNo,
+        type: "GET",
+        success: function (data) {
+
+        }
+    })
+}
+
 //查看档案信息
 $('.edit').on("click",function () {
     var id = $(this).parent().siblings(".id").text();
@@ -5,8 +20,8 @@ $('.edit').on("click",function () {
         $.ajax({
             url: "/information/info/"+id,
             type: "GET",
-            success:function (req) {
-                if (req.success){
+            success:function (data) {
+                if (data.success){
 
                 }
             }
