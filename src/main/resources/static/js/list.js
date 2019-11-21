9345f654f16493476ded214043673a1538317117
Archiver <302866617@qq.com>
2019-11-21 22:14:11 +0800

    通过用户名查询

--- src/main/resources/static/js/list.js
+++ src/main/resources/static/js/list.js
@@ -1,3 +1,21 @@
+//通过姓名查询信息
+$('.search').on("click",function () {
+    var name = $('#search').val();
+    $.ajax({
+        url: "/information/info",
+        type: "GET",
+        data: {"name":name},
+        success:function (data) {
+            if (data.success){
+
+            }else {
+
+            }
+        }
+    })
+})
+
+
 //查看档案信息
 $('.edit').on("click",function () {
     var name = $(this).parent().siblings(".name").text();
