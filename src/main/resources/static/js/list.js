37efa6e30a9bc2bb4d37b70d2d9c8f4c7335ab92
Archiver <302866617@qq.com>
2019-11-23 09:50:29 +0800

    提交

--- src/main/resources/static/js/list.js
+++ src/main/resources/static/js/list.js
@@ -1,5 +1,5 @@
 $(function () {
-    listInfo();
+    listInfo(pageNo);
 })
 
 //通过姓名查询信息
@@ -74,73 +74,106 @@ $(document.body).on('click','.search',function(){
     }
 })
 
+//当前页数
+var pageNo=1;
+var totalPage=0;
+
 // 生成人员列表
-var listInfo = function(){
+var listInfo = function(pageNo){
     $.ajax({
-        url: "/information/info/page/1",
+        url: "/information/info/page/"+pageNo,
         type: "GET",
         success:function (data) {
-            if (data.success){
-                $('.list').html("");
-                var str = "     <tbody>      <tr>\n" +
-                    "                <td colspan=\"4\" style=\"text-align: left\">\n" +
-                    "                    <button class=\"btn btn-primary add\">&nbsp;+添加&nbsp;</button>\n" +
-                    "                </td>\n" +
-                    "                <td  style=\"text-align: left\" colspan=\"2\">\n" +
-                    "                    <input type=\"text\" class=\"form-control\" id=\"search\" value='"+name+"'>\n" +
-                    "                    <button class=\"btn btn-success search\" id=\"btnsearch\">&nbsp;查找&nbsp;</button>\n" +
-                    "                </td>\n" +
-                    "            </tr>\n" +
-                    "            <tr>\n" +
-                    "                <th>\n" +
-                    "                    姓名\n" +
-                    "                </th>\n" +
-                    "                <th>\n" +
-                    "                    性别\n" +
-                    "                </th>\n" +
-                    "                <th >\n" +
-                    "                    出生年月（岁）\n" +
-                    "                </th>\n" +
-                    "                <th>\n" +
-                    "                    专业技术职务\n" +
-                    "                </th>\n" +
-                    "                <th>\n" +
-                    "                    籍贯\n" +
-                    "                </th>\n" +-
-                        "                <th >\n" +
-                    "                    操作\n" +
-                    "                </th>\n" +
-                    "            </tr> ";
-                if (data.obj.length>0){
-                    for (let i=0;i<data.obj.length;i++){
-                        str+= "            <tr>\n" +
-                            "                <td class=\"name\">"+data.obj[i].name+"</td>\n" +
-                            "                <td>"+data.obj[i].sex+"</td>\n" +
-                            "                <td>"+data.obj[i].birthday+"</td>\n" +
-                            "                <td>"+data.obj[i].post+"</td>\n" +
-                            "                <td>"+data.obj[i].nativeplace+"</td>\n" +
-                            "                <td >\n" +
-                            "                    <button class=\"btn btn-primary\" onclick='infor("+data.obj[i].id+")'>查看</button>\n" +
-                            "                    <button class=\"btn btn-danger\" onclick='deleteInfo("+data.obj[i].id+")'>删除</button>\n" +
-                            "                    <button class=\"btn btn-info\" onclick='editInfo("+data.obj[i].id+")'>修改</button>\n" +
-                            "                </td>\n" +
-                            "            </tr>";
-                    }
-                }else {
-                    alert("未找到相关人员信息");
-                    $('.page').remove();
-                    window.location.href="/information/secendhtml/list.html";
-                    return;
-                }
-                str += "</tbody>";
-                $('.list').html(str);
-                $('.page').remove();
-            }else {
-
+            if (data.success) {
+                createList(data);
+                totalPage=data.obj.totalNo;
             }
+        },
+        error: function() {
+            alert("出现错误");
         }
     })
 }
+
+//生成人员列表
+function createList(data){
+    $('.list').html("");
+    var str = "     <tbody>      <tr>\n" +
+        "                <td colspan=\"4\" style=\"text-align: left\">\n" +
+        "                    <button class=\"btn btn-primary add\">&nbsp;+添加&nbsp;</button>\n" +
+        "                </td>\n" +
+        "                <td  style=\"text-align: left\" colspan=\"2\">\n" +
+        "                    <input type=\"text\" class=\"form-control\" id=\"search\" value='"+name+"'>\n" +
+        "                    <button class=\"btn btn-success search\" id=\"btnsearch\">&nbsp;查找&nbsp;</button>\n" +
+        "                </td>\n" +
+        "            </tr>\n" +
+        "            <tr>\n" +
+        "                <th>\n" +
+        "                    姓名\n" +
+        "                </th>\n" +
+        "                <th>\n" +
+        "                    性别\n" +
+        "                </th>\n" +
+        "                <th >\n" +
+        "                    出生年月（岁）\n" +
+        "                </th>\n" +
+        "                <th>\n" +
+        "                    专业技术职务\n" +
+        "                </th>\n" +
+        "                <th>\n" +
+        "                    籍贯\n" +
+        "                </th>\n" +-
+            "                <th >\n" +
+        "                    操作\n" +
+        "                </th>\n" +
+        "            </tr> ";
+    if (data.obj.listInfo.length>0) {
+        for (let i = 0; i < data.obj.listInfo.length; i++) {
+            str += "            <tr>\n" +
+                "                <td class=\"name\">" + data.obj.listInfo[i].name + "</td>\n" +
+                "                <td>" + data.obj.listInfo[i].sex + "</td>\n" +
+                "                <td>" + data.obj.listInfo[i].birthday + "</td>\n" +
+                "                <td>" + data.obj.listInfo[i].post + "</td>\n" +
+                "                <td>" + data.obj.listInfo[i].nativeplace + "</td>\n" +
+                "                <td >\n" +
+                "                    <button class=\"btn btn-primary\" onclick='infor(" + data.obj.listInfo[i].id + ")'>查看</button>\n" +
+                "                    <button class=\"btn btn-danger\" onclick='deleteInfo(" + data.obj.listInfo[i].id + ")'>删除</button>\n" +
+                "                    <button class=\"btn btn-info\" onclick='editInfo(" + data.obj.listInfo[i].id + ")'>修改</button>\n" +
+                "                </td>\n" +
+                "            </tr>";
+        }
+    }
+    else {
+        $('.page').remove();
+        return;
+    }
+    str += "</tbody>";
+    $('.list').html(str);
+}
+
+//上一页
+$(document.body).on("click","#prePage",function () {
+    if (pageNo<=1){
+        $('#prePage').attr('disabled',true);
+    }else {
+        pageNo--;
+        listInfo(pageNo);
+    }
+})
+
+//下一页
+$(document.body).on("click","#nextPage",function () {
+    if (pageNo>=totalPage){
+        $('#nextPage').attr('disabled',true);
+    }else {
+        pageNo++;
+        listInfo(pageNo);
+    }
+})
+
+
+
+
 
 //添加人员信息
 $(document.body).on("click",".add",function () {
@@ -166,6 +199,8 @@ var deleteInfo = function (id) {
             success: function (data) {
                 if (data.success){
                     alert("删除成功");
+                }else {
+                    alert("删除失败");
                 }
             }
         })
